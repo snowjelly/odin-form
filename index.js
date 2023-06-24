5df6a2b669bf1e8a6521489ab358e784d238766d
snowjelly <broadwellconnor@gmail.com>
2023-06-24 14:37:31 -0500

    checkEmailValidity

--- index.js
+++ index.js
@@ -2,9 +2,37 @@ const email = document.querySelector('#email');
 const emailError = document.querySelector('#email + .error');
 const form = document.querySelector('form');
 
-email.addEventListener('focusout', checkValidity);
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+window.addEventListener('load', () => {
+    const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+    if (isValid) {
+        email.className = 'valid';
+        emailError.textContent = '';
+        emailError.className = 'error';
+    }
+    else {
+        email.className = 'invalid';
+    }
 });
 
-const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkEmailValidity();
+});
+
+
+
+function checkEmailValidity() {
+        const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+        if (!isValid) {
+            email.className = 'invalid';
+            emailError.textContent = 'Please enter a valid email';
+            emailError.className = 'error active';
+        }
+        else {
+            email.className = 'valid';
+            emailError.textContent = '';
+            emailError.className = 'error';
+        }
+}

eb82aef4db897cd41010ba69a1c43c53d947ee3d
snowjelly <broadwellconnor@gmail.com>
2023-06-24 14:17:09 -0500

    restart, add email regex

--- index.js
+++ index.js
@@ -2,16 +2,9 @@ const email = document.querySelector('#email');
 const emailError = document.querySelector('#email + .error');
 const form = document.querySelector('form');
 
-email.addEventListener('input', checkValidity);
+email.addEventListener('focusout', checkValidity);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
-function checkValidity() {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity('Please enter in a valid email address');
-    }
-    else if (email.validity.valid) {
-        email.setCustomValidity('');
-    }
-}
+const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;

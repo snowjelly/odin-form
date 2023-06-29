505102b241348e1638a233d9e93cf5b8dec16299
snowjelly <broadwellconnor@gmail.com>
2023-06-29 14:59:59 -0500

    password validity

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const country = document.querySelector('#country');
 const countryError = document.querySelector('#country + .error');
 const zip = document.querySelector('#zip');
 const zipError = document.querySelector('#zip + .error');
+const password = document.querySelector('#password');
+const passwordError = document.querySelector('#password + .error');
 const form = document.querySelector('form');
 const MAX_COUNTRY_LENGTH = 56;
 const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const zipRegExp = /^\d{5}([ \-]\d{4})?/;
+const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+const passwordErrorText = 'password. Your password must include 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character';
 
 window.addEventListener('load', () => {
     displayError(email, emailError, checkEmailValidityOnLoad(), 'email address');
@@ -20,6 +24,7 @@ form.addEventListener('submit', (e) => {
     displayError(email, emailError, checkEmailValidity(), 'email address');
     displayError(country, countryError, checkCountryValidity(), 'country');
     displayError(zip, zipError, checkZipValidity(), 'zip code');
+    displayError(password, passwordError, checkPasswordValidity(), passwordErrorText);
 });
 
 function checkEmailValidityOnLoad() {
@@ -37,6 +42,10 @@ country.addEventListener('focusout', () => {
 
 zip.addEventListener('focusout', () => {
     displayError(zip, zipError, checkZipValidity(), 'zip code');
+});
+
+password.addEventListener('focusout', () => {
+    displayError(password, passwordError, checkPasswordValidity(), passwordErrorText);
 });
 
 function checkEmailValidity() {
@@ -64,6 +73,11 @@ function checkZipValidityOnLoad() {
     return isValid;
 }
 
+function checkPasswordValidity() {
+    const isValid = password.value.length !== 0 && passwordRegExp.test(password.value);
+    return isValid;
+}
+
 function displayError(inputField, inputFieldError, isValid, inputFieldErrorText) {
     if (!isValid) {
         inputField.className = 'invalid';
@@ -75,4 +89,5 @@ function displayError(inputField, inputFieldError, isValid, inputFieldErrorText)
         inputFieldError.textContent = '';
         inputFieldError.className = 'error';
     }
+    return inputField.className;
 }

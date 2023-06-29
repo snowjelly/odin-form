8edc01297de28c6d6f7d0fb2e89adbde08a6f4ff
snowjelly <broadwellconnor@gmail.com>
2023-06-29 13:44:12 -0500

    refactor

--- index.js
+++ index.js
@@ -4,61 +4,56 @@ const country = document.querySelector('#country');
 const countryError = document.querySelector('#country + .error');
 const form = document.querySelector('form');
 const MAX_COUNTRY_LENGTH = 56;
-
 const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 window.addEventListener('load', () => {
-    const isValid = email.value.length === 0 || emailRegExp.test(email.value);
-    if (isValid) {
-        email.className = 'valid';
-        emailError.textContent = '';
-        emailError.className = 'error';
-    }
-    else {
-        email.className = 'invalid';
-    }
+    displayError(email, emailError, checkEmailValidityOnLoad(), 'email address');
+    displayError(country, countryError, checkCountryValidityOnLoad(), 'country');
 });
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    checkEmailValidity();
-    checkCountryValidity();
+    displayError(email, emailError, checkEmailValidity(), 'email address');
+    displayError(country, countryError, checkCountryValidity(), 'country');
 });
 
+function checkEmailValidityOnLoad() {
+    const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+    return isValid;
+}
+
 email.addEventListener('focusout', () => {
-    checkEmailValidity() ;
+    displayError(email, emailError, checkEmailValidity(), 'email address');
 });
 
 country.addEventListener('focusout', () => {
-    checkCountryValidity();
+    displayError(country, countryError, checkCountryValidity(), 'country');
 });
 
 function checkEmailValidity() {
-        const isValid = email.value.length === 0 || emailRegExp.test(email.value);
-        if (!isValid) {
-            email.className = 'invalid';
-            emailError.textContent = 'Please enter a valid email';
-            emailError.className = 'error active';
-        }
-        else {
-            email.className = 'valid';
-            emailError.textContent = '';
-            emailError.className = 'error';
-        }
+        const isValid = email.value.length !== 0 && emailRegExp.test(email.value);
+        return isValid;
 }
 
+function checkCountryValidityOnLoad() {
+        const isValid = country.value.length === 0 || country.value.length <= MAX_COUNTRY_LENGTH;
+        return isValid;
+}
 
 function checkCountryValidity() {
-        const isValid = country.value.length === 0 || country.value.length <= MAX_COUNTRY_LENGTH;
-        console.log(isValid);
-        if (!isValid) {
-            country.className = 'invalid';
-            countryError.textContent = 'Please enter a valid country';
-            countryError.className = 'error active';
-        }
-        else {
-            country.className = 'valid';
-            countryError.textContent = '';
-            countryError.className = 'error';
-        }
+    const isValid = country.value.length !== 0 && country.value.length <= MAX_COUNTRY_LENGTH;
+    return isValid;
+}
+
+function displayError(inputField, inputFieldError, isValid, inputFieldErrorText) {
+    if (!isValid) {
+        inputField.className = 'invalid';
+        inputFieldError.textContent = `Please enter a valid ${inputFieldErrorText}.`;
+        inputFieldError.className = 'error active';
+    }
+    else {
+        inputField.className = 'valid';
+        inputFieldError.textContent = '';
+        inputFieldError.className = 'error';
+    }
 }
